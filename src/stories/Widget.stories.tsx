8076fc54bb4ad2530b2c3ceada299a13d492f256
Luke Rohenaz <rohenaz@gmail.com>
2021-12-22 08:52:43 -0500

    remove unnecessary provider

--- src/stories/Widget.stories.tsx
+++ src/stories/Widget.stories.tsx
@@ -1,7 +1,6 @@
 import { ComponentMeta, Story } from "@storybook/react";
 import React from "react";
 import TonicPowWidget, { WidgetProps } from "../../dist/esm/";
-import { TonicPowProvider } from "../../dist/esm/context/tonicpow";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -14,11 +13,7 @@ export default {
 } as ComponentMeta<typeof TonicPowWidget>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: Story<WidgetProps> = (args) => (
-  <TonicPowProvider>
-    <TonicPowWidget {...args} />
-  </TonicPowProvider>
-);
+const Template: Story<WidgetProps> = (args) => <TonicPowWidget {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
